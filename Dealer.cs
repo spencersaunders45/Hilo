fe25afee8cc725fd54028528095c4fe5f7826acf
Spencer Saunders <spencer.saunders45@gmail.com>
2022-05-13 12:09:10 -0600

    Finished Hilo game

---
+++ Dealer.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hilo;
+
+public class Dealer
+{
+    private Random _random;
+    private bool keepPlaying;
+    private int currentCard;
+    private int oldCard;
+    private Player player;
+    private int score;
+    private string guess;
+
+    public Dealer()
+    {
+        player = new Player();
+        _random = new Random();
+        keepPlaying = true;
+        currentCard = _random.Next(13) + 1;
+        score = 300;
+        guess = "h";
+    }
+
+    public void GameLoop()
+    {
+        while (keepPlaying)
+        {
+            DisplayCurrentCard();
+            guess = player.GuessHilo();
+            DisplayNextCard();
+            CalculateScore();
+            DisplayScore();
+            keepPlaying = (score <= 0) ? false : player.KeepPlaying();
+            Console.WriteLine();
+        }
+    }
+
+    private void DrawCard()
+    {
+        currentCard = _random.Next(13) + 1;
+    }
+
+    private void DisplayCurrentCard()
+    {
+        Console.WriteLine($"The card is: {currentCard}");
+    }
+
+    private void DisplayNextCard()
+    {
+        oldCard = currentCard;
+        DrawCard();
+        Console.WriteLine($"Next card was: {currentCard}");
+    }
+
+    private void CalculateScore()
+    {
+        if (currentCard > oldCard && guess == "h")
+        {
+            score += 100;
+        } else if (currentCard < oldCard && guess == "l")
+        {
+            score += 100;
+        }
+        else
+        {
+            score -= 75;
+        }
+    }
+
+    private void DisplayScore()
+    {
+        Console.WriteLine($"Your score is: {score}");
+    }
+
+}
---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿namespace Hilo;
+
+public class Player
+{
+    public string GuessHilo()
+    {
+        Console.Write("Higher or lower? [h/l] ");
+        string guess = Console.ReadLine();
+        return guess;
+    }
+
+    public bool KeepPlaying()
+    {
+        Console.Write("Play again? [y/n]");
+        string answer = Console.ReadLine();
+        if (answer == "y")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
